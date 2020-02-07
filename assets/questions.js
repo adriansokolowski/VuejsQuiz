55f9edd1c35e45495048615689b043b3723b8189
adriansokolowski <37288976+adriansokolowski@users.noreply.github.com>
2020-02-07 17:48:12 +0100

    Question Structure

---
+++ assets/questions.js
@@ -0,0 +1,45 @@
+const questions = {
+    category: "JavaScript", 
+    question: [
+        {
+            text: "What is this JavaScript?",
+            options: [
+                {text: "Option A", correct: true},
+                {text: "Option B"},
+                {text: "Option C"},
+                {text: "Option D"}
+            ]
+        },
+        {
+            text: "What is this JavaScript 2?",
+            options: [
+                {text: "Option A"},
+                {text: "Option B"},
+                {text: "Option C", correct: true},
+                {text: "Option D"}
+            ]
+        }
+
+    ],
+    category: "PHP",
+    question: [
+        {
+            text: "What is this? PHP",
+            options: [
+                {text: "Option A", correct: true},
+                {text: "Option B"},
+                {text: "Option C"},
+                {text: "Option D"}
+            ]
+        },
+        {
+            text: "What is this 2? PHP",
+            options: [
+                {text: "Option A"},
+                {text: "Option B"},
+                {text: "Option C", correct: true},
+                {text: "Option D"}
+            ]
+        }
+    ]
+}
