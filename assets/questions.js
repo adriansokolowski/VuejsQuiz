89a8629f12833bf2f0fec1e3eec28416258b52ee
adriansokolowski <37288976+adriansokolowski@users.noreply.github.com>
2020-02-12 04:41:04 +0100

    Add pagination logic

--- assets/questions.js
+++ assets/questions.js
@@ -1,45 +1,83 @@
-const questions = {
-    category: "JavaScript", 
-    question: [
-        {
-            text: "What is this JavaScript?",
-            options: [
-                {text: "Option A", correct: true},
-                {text: "Option B"},
-                {text: "Option C"},
-                {text: "Option D"}
-            ]
-        },
-        {
-            text: "What is this JavaScript 2?",
-            options: [
-                {text: "Option A"},
-                {text: "Option B"},
-                {text: "Option C", correct: true},
-                {text: "Option D"}
-            ]
-        }
+const questions = [
+    {
+        category: "JavaScript",
+        question: [
+            {
+                text: "What is difference between var let and const?",
+                options: [
+                    { text: "Scope", correct: true },
+                    { text: "Option B" },
+                    { text: "Option C" },
+                    { text: "Option D" }
+                ]
+            },
+            {
+                text: "What is this JavaScript 2?",
+                options: [
+                    { text: "Option A" },
+                    { text: "Option B" },
+                    { text: "Option C", correct: true },
+                    { text: "Option D" }
+                ]
+            },
+            {
+                text: "What is this JavaScript 2?",
+                options: [
+                    { text: "Option A" },
+                    { text: "Option B" },
+                    { text: "Option C", correct: true },
+                    { text: "Option D" }
+                ]
+            }
 
-    ],
-    category: "PHP",
-    question: [
-        {
-            text: "What is this? PHP",
-            options: [
-                {text: "Option A", correct: true},
-                {text: "Option B"},
-                {text: "Option C"},
-                {text: "Option D"}
-            ]
-        },
-        {
-            text: "What is this 2? PHP",
-            options: [
-                {text: "Option A"},
-                {text: "Option B"},
-                {text: "Option C", correct: true},
-                {text: "Option D"}
-            ]
-        }
-    ]
-}
+        ]
+    },
+    {
+        category: "PHP",
+        question: [
+            {
+                text: "What is this? PHP",
+                options: [
+                    { text: "Option A", correct: true },
+                    { text: "Option B" },
+                    { text: "Option C" },
+                    { text: "Option D" }
+                ]
+            },
+            {
+                text: "What is this 2? PHP",
+                options: [
+                    { text: "Option A" },
+                    { text: "Option B" },
+                    { text: "Option C", correct: true },
+                    { text: "Option D" }
+                ]
+            }
+        ]
+    },
+    {
+        category: "Logiczne",
+        question: [
+            {
+                text: "What is this? logiczne",
+                options: [
+                    { text: "Option A", correct: true },
+                    { text: "Option B" },
+                    { text: "Option C" },
+                    { text: "Option D" }
+                ]
+            },
+            {
+                text: "What is this 2? logiczne",
+                options: [
+                    { text: "Option A" },
+                    { text: "Option B" },
+                    { text: "Option C", correct: true },
+                    { text: "Option D" }
+                ]
+            }
+        ]
+    }
+]
+
+console.log(questions[2].question[1].options);
--- assets/quiz.js
+++ assets/quiz.js
@@ -1,3 +1,29 @@
-class questions {
-    
-}
+
+const quiz = new Vue({
+    el: "#quiz",
+    data: {
+        questions: questions,
+        currentQuestion: 0,
+        currentCategory: 0,
+        userResponses: Array(questions[0].length).fill(null)
+    },
+    methods: {
+        fillAnswers: function(){
+            userResponses = Array(questions[this.currentCategory].length).fill(null)
+        },
+
+        next: function(){
+            if (this.currentQuestion < questions[this.currentCategory].question.length-1 ){
+                this.currentQuestion++;
+            }
+        },
+
+        previous: function(){
+
+            if (this.currentQuestion > 0 ){
+                this.currentQuestion--;
+            }
+        }
+    }
+})
+
